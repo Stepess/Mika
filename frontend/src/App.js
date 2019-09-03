fe9bcdd0ff851c9c5aaeadd0c367e6a071b2c996
Stepess <stepanersh@gmail.com>
2019-09-03 10:39:48 +0300

    cleanup frontend project and format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
-
-// const React = require('react');
-// const ReactDOM = require('react-dom');
-//const client = require('./client');
 const axios = require('axios');
 
 class App extends React.Component {
@@ -50,9 +25,8 @@ class App extends React.Component {
     }
 }
 
-class UserList extends React.Component{
-	render() {
-		const users = this.props.users.map(user =>
+function UserList(props){
+		const users = props.users.map(user =>
 			<User key={user.id} user={user}/>
 		);
 		return (
@@ -68,18 +42,15 @@ class UserList extends React.Component{
 			</table>
 		)
 	}
-}
 
-class User extends React.Component{
-	render() {
+function User(props){
 		return (
 			<tr>
-        <td>{this.props.user.username}</td>
-				<td>{this.props.user.firstName}</td>
-				<td>{this.props.user.email}</td>
+        <td>{props.user.username}</td>
+				<td>{props.user.firstName}</td>
+				<td>{props.user.email}</td>
 			</tr>
 		)
 	}
-}
 
 export default App;
