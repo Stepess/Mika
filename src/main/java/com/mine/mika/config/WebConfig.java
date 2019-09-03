687372d466fde7889ccfcf03ea75007f3ba1de1b
Stepess <stepanersh@gmail.com>
2019-09-03 10:12:41 +0300

    add cors support

--- src/main/java/com/mine/mika/config/WebConfig.java
+++ src/main/java/com/mine/mika/config/WebConfig.java
@@ -1,13 +1,23 @@
 package com.mine.mika.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/users/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .maxAge(1800);
     }
 }
